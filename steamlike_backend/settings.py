1ba524cde12210ffc3fa988bb51011ed90d07472
DavidGranadosCruz <davidgr871@gmail.com>
2026-04-27 09:07:33 +0200

    fix: usar STORAGES en vez de STATICFILES_STORAGE y STATIC_URL con slash

--- steamlike_backend/settings.py
+++ steamlike_backend/settings.py
@@ -95,10 +95,19 @@ TIME_ZONE = "Europe/Madrid"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # --- CORS + cookies (SessionAuthentication) ---
